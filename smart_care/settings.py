a234affe537a4076f7a9bde4be4189182b6da2ca
morsalin-islam335 <weshallovercome335@gmail.com>
2024-04-30 20:53:18 +0600

    done

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -28,6 +28,10 @@ SECRET_KEY = env("KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+CSRF_TRUSTED_ORIGINS = ['https://yhospital.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
